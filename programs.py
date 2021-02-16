563c4c6b27f5a5391fc1099341097ad4bfcca4cc
Rehan Patel <34427014+rehansp@users.noreply.github.com>
2021-02-16 12:30:34 +0530

    Update programs.py

--- programs.py
+++ programs.py
@@ -1,1 +1,21 @@
+# reverse list without using inbuilt method
+print(f'previo')
+x = [1,2,3,4]
+l = len(x)
+x2 = []
 
+i = -1
+while(i>=-l):
+  x2.append(x[i])
+  print(x2)
+  i-=1
+
+
+# string reverse without using inbuilt method
+x = "python"
+l = len(x)
+
+i = -1
+while(i>=-l):
+  print(x[i])
+  i -=1
